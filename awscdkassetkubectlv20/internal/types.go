456fc66dbe39d749b659f70023fcc533b8a730f2
AWS CDK Team <aws-cdk@amazon.com>
2022-09-28 05:37:18 +0000

    chore(release): v0.0.1

--- awscdkassetkubectlv20/internal/types.go
+++
@@ -1,5 +0,0 @@
-package internal
-import (
-	"github.com/aws/aws-cdk-go/awscdk/v2/awss3assets"
-)
-type Type__awss3assetsAsset = awss3assets.Asset
---
+++ kubectlv20/kubectlv20_KubectlAsset__no_runtime_type_checking.go
@@ -0,0 +1,24 @@
+//go:build no_runtime_type_checking
+// +build no_runtime_type_checking
+
+// An Asset construct that contains kubectl, for use in Lambda Layers
+package kubectlv20
+
+// Building without runtime type checking enabled, so all the below just return nil
+
+func (k *jsiiProxy_KubectlAsset) validateAddResourceMetadataParameters(resource awscdk.CfnResource, resourceProperty *string) error {
+	return nil
+}
+
+func (k *jsiiProxy_KubectlAsset) validateGrantReadParameters(grantee awsiam.IGrantable) error {
+	return nil
+}
+
+func validateKubectlAsset_IsConstructParameters(x interface{}) error {
+	return nil
+}
+
+func validateNewKubectlAssetParameters(scope constructs.Construct, id *string, options *awss3assets.AssetOptions) error {
+	return nil
+}
+
